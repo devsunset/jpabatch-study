8d11f76858f2cdc57c4edf6b9b5593e069ef48db
devsunset <devsunset@gmail.com>
2021-07-12 15:32:32 +0900

    dev

--- src/main/java/com/example/jpabatch/sample/repository/PayRepository.java
+++ src/main/java/com/example/jpabatch/sample/repository/PayRepository.java
@@ -4,8 +4,9 @@ package com.example.jpabatch.sample.repository;
 
 import com.example.jpabatch.sample.domain.Pay;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface PayRepository extends JpaRepository<Pay,Long> {
+
+public interface PayRepository extends CrudRepository<Pay,Long> {
 
 }
