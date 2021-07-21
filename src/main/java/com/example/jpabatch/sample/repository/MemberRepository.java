5f5a8963f6466f36b2c7e14179393335433290b8
devsunset <devsunset@gmail.com>
2021-07-21 13:33:11 +0900

    dev

--- src/main/java/com/example/jpabatch/sample/repository/MemberRepository.java
+++ src/main/java/com/example/jpabatch/sample/repository/MemberRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+//    jpa.query-methods.query-creation
+//    https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
     Member findByEmail(String email);
 
 }
