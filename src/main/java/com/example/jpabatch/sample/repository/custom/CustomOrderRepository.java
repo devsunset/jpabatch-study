52b007a9b1158a540633aad32b56ae1666808b2f
devsunset <devsunset@gmail.com>
2021-07-26 16:48:40 +0900

    dev - source copy

---
+++ src/main/java/com/example/jpabatch/sample/repository/custom/CustomOrderRepository.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.repository.custom;
+
+import com.example.jpabatch.sample.entity.Order;
+import com.example.jpabatch.sample.entity.OrderSearch;
+
+import java.util.List;
+
+/**
+ * @author holyeye
+ */
+public interface CustomOrderRepository {
+
+    public List<Order> search(OrderSearch orderSearch);
+
+}
